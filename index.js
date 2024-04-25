a4125d07d6c5c9d677e3085dcf88119b07ea4513
PekiPho <v.a.r.d.games123@gmail.com>
2024-04-26 00:43:26 +0200

    Added load factor array increase

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 class HashMap{
 
     constructor(size){
-        this.data=new Array(size);
+        this.data=new Array(size).fill(undefined);
         const loadFactor=0.75;
     }
 
@@ -24,8 +24,14 @@ class HashMap{
     set(key,value){
         let h= this.hash(key)%this.data.length;
         let i=1;
+        if(this.length()>=0.75*this.data.length)
+        {
+            this.data.length*=2;
+            this.data.fill(undefined,this.data.length/2,this.data.length);
+        }
+            
 
-        while(this.data[h]!=null){
+        while(this.data[h]!=undefined){
             if(key == this.data[h][0]){
                 this.data[h]=[key,value];
                 return this.data;
@@ -42,14 +48,13 @@ class HashMap{
         let h=this.hash(key)%this.data.length;
         let i=1;
 
-        if(this.data[h]==undefined)
-            return null;
-
-        while(this.data[h][0]!=key){
+        while(this.data[h]!== undefined&& key!=this.data[h][0]){
             h=(h+this.secondary(i++))%this.data.length;
             if(i>=this.data.length)
                 return null;
         }
+        if(this.data[h]===undefined)
+            return null;
         return this.data[h][1];
     }
 
@@ -57,26 +62,38 @@ class HashMap{
         var h=this.hash(key)%this.data.length;
         var i=1;
 
-        if(this.data[h]==undefined)
-            return false;
 
-        while(key!=this.data[h][0]){
+        while(this.data[h]!== undefined&& key!=this.data[h][0] ){
             h=(h+this.secondary(i++))%this.data.length;
             if(i>=this.data.length)
                 return false;
         }
+        if(this.data[h]===undefined)
+            return false;
         return true;
     }
 
     remove(key){
+        var h=this.hash(key)%this.data.length;
+        var i=1;
 
+
+        while(this.data[h]!== undefined&& key!=this.data[h][0]){
+            h=(h+this.secondary(i++))%this.data.length;
+            if(i>=this.data.length)
+                return false;
+        }
+        if(this.data[h]==undefined)
+            return false;
+        this.data[h]=undefined;
+        return true;
     }
 
     length(){
         var count = 0;
         for(var i=0;i<this.data.length;i++)
         {
-            if(this.data[i]!=null)
+            if(this.data[i]!=undefined)
             {
                 count++;
             }
@@ -86,8 +103,8 @@ class HashMap{
 
     clear(){
         for(var i=0;i<this.data.length;i++){
-            if(this.data[i]!=null)
-                this.data[i]=null;
+            if(this.data[i]!=undefined)
+                this.data[i]=undefined;
         }
     }
 
@@ -96,7 +113,7 @@ class HashMap{
         var i=0;
         for(var j=0;j<this.data.length;j++)
         {
-            if(this.data[j]!=null)
+            if(this.data[j]!=undefined)
             {
                 keyArr[i++]=this.data[j][0];
             }
@@ -109,7 +126,7 @@ class HashMap{
         var i=0;
         for(var j=0;j<this.data.length;j++)
         {
-            if(this.data[j]!=null)
+            if(this.data[j]!=undefined)
             {
                 keyArr[i++]=this.data[j][1];
             }
@@ -118,7 +135,16 @@ class HashMap{
     }
 
     entries(){
-
+        var pairArr= new Array();
+        var i=0;
+        for(var j=0;j<this.data.length;j++)
+        {
+            if(this.data[j]!=undefined)
+            {
+                pairArr[i++]=this.data[j];
+            }
+        }
+        return pairArr;
     }
 }
 
@@ -126,12 +152,25 @@ let map = new HashMap(8);
 map.set("Carlos",3);
 map.set("Luke",5);
 map.set("Carld",4);
+map.set("bb",1);
+map.set("Mike",2);
+map.set("Jenny",9);
+console.log(map.length());
+map.set("Michael",10);
 
 console.log(map.length()); // 3
 console.log(map.keys()); //[Luke,Carlos,Carla]
-console.log(map.has("Carld"));
+console.log(map.has("Carlo"));
 console.log(map.get("Carld"));
+map.remove("Carld");
+map.remove("carl");
+console.log(map.has("Carld"));
+map.set("Carld",6);
+map.set("Carld",9);
 
+
+
+console.log(map.entries()); // array of objects
 // console.log(map.values()); //[5,3,4]
 // console.log(map.has("Carlos")); //true
 // console.log(map.has("Carlo")); //false
